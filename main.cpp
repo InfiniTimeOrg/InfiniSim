c6bccf52252b1199a995a0896775e6159b20054f
Riku Isokoski <riksu9000@gmail.com>
2023-02-24 18:05:33 +0200

    Apply InfiniTime TouchHandler changes

--- main.cpp
+++ main.cpp
@@ -341,7 +341,7 @@ Pinetime::Controllers::NotificationManager notificationManager;
 Pinetime::Controllers::MotionController motionController;
 Pinetime::Controllers::TimerController timerController;
 Pinetime::Controllers::AlarmController alarmController {dateTimeController};
-Pinetime::Controllers::TouchHandler touchHandler(touchPanel, lvgl);
+Pinetime::Controllers::TouchHandler touchHandler;
 Pinetime::Controllers::ButtonHandler buttonHandler;
 Pinetime::Controllers::BrightnessController brightnessController {};
 
